3010cdf0223c34a3d212c5fb1a5d27fc8aff3c47
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-06-21 23:21:35 +0400

    change directories structure

--- src/main.ml
+++ src/main.ml
@@ -3,22 +3,22 @@ let print_option_int opt =
   match opt with
   | Some item -> print_int item
   | None -> print_endline "None";;
-(* Running pr1 *)
-Pr1.last [1; 2; 3];;            (* Some 3 *)
-(* Running pr2 *)
-Pr2.last_two [1; 2; 3];;        (* Some (2, 3) *)
-(* Running pr3 *)
-Pr3.at 2 [50; 2; 3; 4];;        (* Some 3 *)
-(* Running pr4 *)
-Pr4.length [1; 2; 3; 4];;       (* 4 *)
-(* Running pr5 *)
-Pr5.rev [1; 2; 3; 4];;          (* [4; 3; 2; 1] *)
-(* Running pr6 *)
-Pr6.is_palindrome [1; 2; 2; 1];; (* true *)
-(* Running pr7 *)
+(* Running list_last *)
+List_last.last [1; 2; 3];;            (* Some 3 *)
+(* Running list_last_two *)
+List_last_two.last_two [1; 2; 3];;        (* Some (2, 3) *)
+(* Running list_k_element *)
+List_k_element.at 2 [50; 2; 3; 4];;        (* Some 3 *)
+(* Running list_length *)
+List_length.length [1; 2; 3; 4];;       (* 4 *)
+(* Running list_reverse *)
+List_reverse.rev [1; 2; 3; 4];;          (* [4; 3; 2; 1] *)
+(* Running list_palindrome *)
+List_palindrome.is_palindrome [1; 2; 2; 1];; (* true *)
+(* Running list_flatten *)
 type 'a node =
     | One of 'a 
     | Many of 'a node list;;
-Pr7.flatten [ One "a" ;
+List_flatten.flatten [ One "a" ;
               Many [ One "b" ; Many [ One "c" ; One "d" ] ; One "e" ];
             ];;
