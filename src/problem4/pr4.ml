4f2cac91f76ed6557d2274a156e0acf3719a7a15
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-06-21 14:40:50 +0400

    putting one function in another problem4

--- src/problem4/pr4.ml
+++ src/problem4/pr4.ml
@@ -1,7 +1,6 @@
-let rec count_length list k =
+let length list =
+  let rec count_length list k =
   match list with
   | [] -> k
-  | hd :: tl ->
-    count_length tl (k+1);;
-let length list =
+  | hd :: tl -> count_length tl (k+1) in
   count_length list 0;;
