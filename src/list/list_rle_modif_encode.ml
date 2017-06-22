c05dfb1d3e3d2af683f2f86c670a7ab6774b7695
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-06-22 18:08:21 +0400

    move some logic to another function, rle modif prob

--- src/list/list_rle_modif_encode.ml
+++ src/list/list_rle_modif_encode.ml
@@ -2,15 +2,13 @@ type 'a rle =
     | One of 'a
     | Many of int * 'a;;
 let encode list =
+  let make_tuple elem count =
+    if count = 1 then One elem
+    else Many (count, elem) in
   let rec aux acc count = function
     | [] -> acc
-    | a :: [] ->
-      if count = 0 then acc @ [One a]
-      else acc @ [Many (count+1, a)]
+    | a :: [] -> acc @ [make_tuple a (count+1)]
     | a :: (b :: _ as tl) ->
       if a = b then aux acc (count+1) tl
-      else (
-        if count = 0 then aux (acc @ [One a]) 0 tl
-        else aux (acc @ [Many (count+1, a)]) 0 tl
-      ) in
+      else aux (acc @ [make_tuple a (count+1)]) 0 tl in
   aux [] 0 list
