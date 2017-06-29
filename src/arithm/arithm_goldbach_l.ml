2689b1e12765459349f184f622d3eaa090f80c2e
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-06-29 17:28:12 +0400

    much smarter implementation of limit goldbach arithm

--- src/arithm/arithm_goldbach_l.ml
+++ src/arithm/arithm_goldbach_l.ml
@@ -6,13 +6,5 @@ let goldbach_list st fin =
       aux (acc @ [ (n, tup) ]) (n+1)
     )else aux acc (n+1) in
   aux [] st;;
-let goldbach_limit st fin lim =
-  let rec aux acc n =
-    if n > fin then acc
-    else if n mod 2 = 0 then (
-      let tup = Arithm_goldbach.goldbach n in
-      let a, b = tup in
-      if a > 50 && b > 50 then aux (acc @ [ (n, tup) ]) (n+1)
-      else aux acc (n+1)
-    )else aux acc (n+1) in
-  aux [] st;;
+let goldbach_limit a b lim =
+    List.filter (fun (_,(a,b)) -> a > lim && b > lim) (goldbach_list a b);;
