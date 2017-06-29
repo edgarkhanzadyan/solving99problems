8fada43eeaabb51a6fcaa46040077514bb6555d5
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-06-29 16:41:06 +0400

    solve goldbach conjecture

---
+++ src/arithm/arithm_goldbach.ml
@@ -0,0 +1,6 @@
+let goldbach num =
+  let rec aux n =
+    if Arithm_prime.is_prime n && Arithm_prime.is_prime (num - n) then
+      (n, num-n)
+    else aux (n+1) in
+  aux 1;;
--- src/main.ml
+++ src/main.ml
@@ -72,3 +72,4 @@ Arithm_phi_impr.phi_improved 10;;
 Arithm_timeit.timeit Arithm_phi.phi 10090;;
 Arithm_timeit.timeit Arithm_phi_impr.phi_improved 10090;;
 List.length (Arithm_all_primes.all_primes 2 7920);;
+Arithm_goldbach.goldbach 28;;
